1f9991d181eea8267151989fbc63b86ecc4ca77e
Mconway409 <70673286+Mconway409@users.noreply.github.com>
2020-12-03 17:30:08 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ $(".saveBtn").on("click", function () {
     console.log(info, time);
 });
 
-
+$("#8oclock").val(localStorage.getItem("8oclock"));
+$("#9oclock").val(localStorage.getItem("9oclock"));
+$("#10oclock").val(localStorage.getItem("10oclock"));
+$("#11oclock").val(localStorage.getItem("11oclock"));
+$("#12oclock").val(localStorage.getItem("12oclock"));
+$("#13oclock").val(localStorage.getItem("13oclock"));
+$("#14oclock").val(localStorage.getItem("14oclock"));
+$("#15oclock").val(localStorage.getItem("15oclock"));
+$("#16oclock").val(localStorage.getItem("16oclock"));
+$("#17oclock").val(localStorage.getItem("17oclock"));
+$("#18oclock").val(localStorage.getItem("18oclock"));
 
 function time() {
     var currentHour = moment().hours();
