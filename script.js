0fdfacdaab396e4f5e5ea222dc4e25d69154664b
Mconway409 <70673286+Mconway409@users.noreply.github.com>
2020-12-03 14:54:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,36 +3,26 @@ $(document).ready(function () {
 $("#currentDay").text(moment().format('dddd') + ", " + moment().format("MMMM Do YYYY, h:mm:ss a"))
 // local storage
 $(".saveBtn").on("click", function () {
-    console.log(this);
-    var text = $(this).siblings(".form-control").val();
+    var info = $(this).siblings(".form-control").val();
     var time = $(this).parent().attr("id");
-
-    localStorage.setItem(time, text);
+    localStorage.setItem(info, time)
+    console.log(this);
 });
 
-$("#8am .form-control").val(localStorage.getItem("8am"));
-$("#9am .form-control").val(localStorage.getItem("9am"));
-$("#10am .form-control").val(localStorage.getItem("10am"));
-$("#11am .form-control").val(localStorage.getItem("11am"));
-$("#12pm .form-control").val(localStorage.getItem("12pm"));
-$("#1pm .form-control").val(localStorage.getItem("1pm"));
-$("#2pm .form-control").val(localStorage.getItem("2pm"));
-$("#3pm .form-control").val(localStorage.getItem("3pm"));
-$("#4pm .form-control").val(localStorage.getItem("4pm"));
-$("#5pm .form-control").val(localStorage.getItem("5pm"));
-$("#6pm .form-control").val(localStorage.getItem("6pm"));
 
 function time() {
     var currentHour = moment().hours();
 
     $(".form-control").each(function () {
-        var hour = parseInt($(this).attr("id"));
-        if (hour < currentHour) {
+        var timeSlot = parseInt($(this).attr("id"));
+        if (timeSlot < currentHour) {
             $(this).addClass("past");
+            
         }
-        else if (hour === currentHour) {
+        else if (timeSlot === currentHour) {
             $(this).removeClass("past");
             $(this).addClass("present");
+            $(this).removeClass("future");
         }
         else {
             $(this).removeClass("past");
